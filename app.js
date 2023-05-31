444db29162d4059c2fbeedb0c95e25c361359008
Uche <133172343+ucheidm@users.noreply.github.com>
2023-06-01 00:59:49 +0100

    manipulating of weather forecast using JavaScript

--- app.js
+++ app.js
@@ -25,6 +25,33 @@ function formatTime(timestamp) {
   return `${weekday} ${hours}:${minutes}`;
 }
 
+function showForecast() {
+  let forecast = document.querySelector("#forecast");
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+
+  let forecastHTML = `<div class ="row">`;
+  days.forEach(function (weekday) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col">
+      <div class = "weather-days">${weekday}</div>
+      
+      <img
+        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
+        id="icon"
+      />
+      <p class="day">
+        34° <small>25°</small>
+      </p>
+    </div>
+  
+`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecast.innerHTML = forecastHTML;
+}
+
 function currentCity(response) {
   let city = document.querySelector("#cityName");
   city.innerHTML = response.data.name;
@@ -95,3 +122,5 @@ let celsiusLink = document.querySelector("#Celsius-link");
 celsiusLink.addEventListener("click", showCelsius);
 
 searching("Nigeria");
+
+showForecast();
